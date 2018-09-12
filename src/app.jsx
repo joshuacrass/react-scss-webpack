99ed1363bb4fb8b21253fd0626bda035d69345ff
Joshua Crass <joshua.crass@gmail.com>
2018-09-12 09:01:02 +0200

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import Button from 'Components/button';
+import style from './app.css';
+
+const App = () => (
+  <div className={style.app}>
+    React Starter
+    <span role="img" aria-label="rocket ship">
+      🚀
+    </span>
+    <Button />
+  </div>
+);
+
+export default hot(module)(App);
